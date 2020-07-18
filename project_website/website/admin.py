fe2e1541c917a3326968da86820b184a7d2fc5dd
ellali824 <57321296+ellali824@users.noreply.github.com>
2020-07-18 19:31:35 -0400

    almost done

--- project_website/website/admin.py
+++ project_website/website/admin.py
@@ -3,14 +3,14 @@ from . import models
 
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'technology', 'image')
+    list_display = ('title', 'description', 'technology', 'image', 'link')
     list_filter = ('title', )
     ordering = ('title', )
     search_field = ('title')
 admin.site.register(models.Project, ProjectAdmin)
 
 class WorkAdmin(admin.ModelAdmin):
-    list_display = ('title', 'description', 'technology', 'image')
+    list_display = ('title', 'description', 'description2', 'image', 'link')
     list_filter = ('title', )
     ordering = ('title', )
     search_field = ('title')
--- project_website/website/models.py
+++ project_website/website/models.py
@@ -4,12 +4,14 @@ from django.db import models
 # model for my projects 
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.CharField(max_length=100)
     technology = models.CharField(max_length=100)
     image = models.ImageField(upload_to='gallery')
+    link = models.URLField(max_length=200, null=True)
 
 class Work(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    description2 = models.CharField(max_length=100)
     image = models.ImageField(upload_to='gallery')
+    link = models.URLField(max_length=200, null = True)
