95943277957dc53f120c139cb6667d21dd21e574
ellali824 <57321296+ellali824@users.noreply.github.com>
2020-08-04 15:42:54 -0700

    updated project/work font size

--- project_website/website/views.py
+++ project_website/website/views.py
@@ -15,7 +15,7 @@ def project_index(request):
     context = { # The dictionary only has one entry projects to which we assign our Queryset containing all projects. The context dictionary is used to send information to our template. Every view function you create needs to have a context dictionary.
         'projects': projects, 'url': url
     }
-    return render(request, 'project_index.html',context) # Any entries in the context dictionary are available in the template, as long as the context argument is passed to render(). You’ll need to create a context dictionary and pass it to render in each view function you create.
+    return render(request, 'project_index.html', context) # Any entries in the context dictionary are available in the template, as long as the context argument is passed to render(). You’ll need to create a context dictionary and pass it to render in each view function you create.
 
 def work(request):
     works = Work.objects.all() # perform a query (a command that allows you to create, retrieve, update, or delete objects (or rows) in your database. In this case, you’re retrieving all objects in the projects table.)    
@@ -23,5 +23,5 @@ def work(request):
     context = { # The dictionary only has one entry projects to which we assign our Queryset containing all projects. The context dictionary is used to send information to our template. Every view function you create needs to have a context dictionary.
         'works': works, 'url': url
     }
-    return render(request, 'work.html',context) # Any entries in the context dictionary are available in the template, as long as the context argument is passed to render(). You’ll need to create a context dictionary and pass it to render in each view function you create.
+    return render(request, 'work.html', context) # Any entries in the context dictionary are available in the template, as long as the context argument is passed to render(). You’ll need to create a context dictionary and pass it to render in each view function you create.
     
